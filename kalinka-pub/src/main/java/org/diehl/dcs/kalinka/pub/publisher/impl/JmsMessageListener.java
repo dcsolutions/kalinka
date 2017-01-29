88c748f9a21e1a8c29129766235b6a7311dadf50
michas <michas@jarmoni.org>
2017-01-29 20:33:24 +0100

    added license

--- kalinka-pub/src/main/java/org/diehl/dcs/kalinka/pub/publisher/impl/JmsMessageListener.java
+++ kalinka-pub/src/main/java/org/diehl/dcs/kalinka/pub/publisher/impl/JmsMessageListener.java
@@ -1,3 +1,19 @@
+/*
+Copyright [2017] [DCS <Info-dcs@diehl.com>]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package org.diehl.dcs.kalinka.pub.publisher.impl;
 
 import javax.jms.Message;
@@ -22,10 +38,8 @@ public class JmsMessageListener<K, V> implements MessageListener {
 	}
 
 	@Override
-	public void onMessage(final Message arg0) {
+	public void onMessage(final Message msg) {
 
-		this.messageToKafkaPublisher.publish(arg0, this.kafkaTemplate);
-		// TODO Auto-generated method stub
-
+		this.messageToKafkaPublisher.publish(msg, this.kafkaTemplate);
 	}
 }
