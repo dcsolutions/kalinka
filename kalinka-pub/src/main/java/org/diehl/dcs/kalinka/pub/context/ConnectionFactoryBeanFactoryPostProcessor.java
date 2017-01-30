4f1be17d4fb3311b7c6e2afcec51c3af9ed7945b
michas <michas@jarmoni.org>
2017-01-30 20:55:15 +0100

    some refactoring

--- kalinka-pub/src/main/java/org/diehl/dcs/kalinka/pub/context/ConnectionFactoryBeanFactoryPostProcessor.java
+++ kalinka-pub/src/main/java/org/diehl/dcs/kalinka/pub/context/ConnectionFactoryBeanFactoryPostProcessor.java
@@ -1,3 +1,19 @@
+/*
+Copyright [2017] [DCS <Info-dcs@diehl.com>]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package org.diehl.dcs.kalinka.pub.context;
 
 import java.util.List;
@@ -14,18 +30,23 @@ import org.springframework.jms.listener.DefaultMessageListenerContainer;
 
 import com.google.common.base.Splitter;
 
+/**
+ * @author michas <michas@jarmoni.org>
+ *
+ */
 public class ConnectionFactoryBeanFactoryPostProcessor implements BeanDefinitionRegistryPostProcessor {
 
 	private final List<String> jmsHosts;
 	private final List<String> jmsDestinations;
 	private final String kafkaClientIdPrefix;
 
+	// Found no better way to configure multiple ConnectionFactories dynamically
+	// See: https://dzone.com/articles/how-create-your-own-dynamic
 	public ConnectionFactoryBeanFactoryPostProcessor(final Environment springEnvironment) {
 
 		this.jmsHosts = Splitter.on(',').omitEmptyStrings().trimResults().splitToList(springEnvironment.getProperty("jms.hosts"));
 		this.jmsDestinations = Splitter.on(',').omitEmptyStrings().trimResults().splitToList(springEnvironment.getProperty("jms.destinations"));
 		this.kafkaClientIdPrefix = springEnvironment.getProperty("kafka.client.id.prefix");
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -37,22 +58,23 @@ public class ConnectionFactoryBeanFactoryPostProcessor implements BeanDefinition
 	@Override
 	public void postProcessBeanDefinitionRegistry(final BeanDefinitionRegistry registry) throws BeansException {
 
-		for (final String jmsHost : this.jmsHosts) {
-			final BeanDefinitionBuilder definitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(CachingConnectionFactory.class);
-			definitionBuilder.addConstructorArgValue(new ActiveMQConnectionFactory(jmsHost));
-			definitionBuilder.addPropertyValue("cacheConsumers", true);
-			definitionBuilder.addPropertyValue("clientId", this.kafkaClientIdPrefix);
-			registry.registerBeanDefinition("connectionFactory" + jmsHost, definitionBuilder.getBeanDefinition());
-			for (final String dest : this.jmsDestinations) {
+		this.jmsHosts.forEach(jmsHost -> {
+
+			final BeanDefinitionBuilder connectionFactoryBuilder = BeanDefinitionBuilder.genericBeanDefinition(CachingConnectionFactory.class);
+			connectionFactoryBuilder.addConstructorArgValue(new ActiveMQConnectionFactory(jmsHost));
+			connectionFactoryBuilder.addPropertyValue("cacheConsumers", true);
+			connectionFactoryBuilder.addPropertyValue("clientId", this.kafkaClientIdPrefix);
+			registry.registerBeanDefinition("connectionFactory-" + jmsHost, connectionFactoryBuilder.getBeanDefinition());
+
+			this.jmsDestinations.forEach(dest -> {
+
 				final BeanDefinitionBuilder listenerBuilder = BeanDefinitionBuilder.genericBeanDefinition(DefaultMessageListenerContainer.class);
 				listenerBuilder.addPropertyReference("messageListener", "messageListener");
-				listenerBuilder.addPropertyReference("connectionFactory", "connectionFactory" + jmsHost);
+				listenerBuilder.addPropertyReference("connectionFactory", "connectionFactory-" + jmsHost);
 				listenerBuilder.addPropertyValue("destinationName", dest);
-				registry.registerBeanDefinition("messageListenerContainer_" + jmsHost + "_" + dest, listenerBuilder.getBeanDefinition());
-			}
-		}
-		// TODO Auto-generated method stub
+				registry.registerBeanDefinition("messageListenerContainer-" + jmsHost + "-" + dest, listenerBuilder.getBeanDefinition());
 
+			});
+		});
 	}
-
 }
---
+++ kalinka-pub/src/main/java/org/diehl/dcs/kalinka/pub/context/JmsApplicationContextInitializer.java
@@ -0,0 +1,35 @@
+/*
+Copyright [2017] [DCS <Info-dcs@diehl.com>]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package org.diehl.dcs.kalinka.pub.context;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @author michas <michas@jarmoni.org>
+ *
+ */
+public class JmsApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+	@Override
+	public void initialize(final ConfigurableApplicationContext applicationContext) {
+
+		final ConfigurableEnvironment env = applicationContext.getEnvironment();
+		applicationContext.addBeanFactoryPostProcessor(new ConnectionFactoryBeanFactoryPostProcessor(env));
+	}
+}
